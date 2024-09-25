97df99036ef776df9576c9a1c1be4c6c9ec617a2
Michael Kanu <michaelokanu01@yahoo.com>
2024-09-25 21:51:18 +0000

    set-and-get

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+// script that logs to the console "Redis client connect
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error.message}`);
+});
+
+const setNewSchool = (schoolName, value) => {
+  client.SET(schoolName, value, print);
+};
+
+const displaySchoolValue = (schoolName) => {
+  client.GET(schoolName, (_err, reply) => {
+    console.log(reply);
+  });
+};
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
